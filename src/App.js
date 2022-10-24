f5cdab28ea8fe9055f33cf24ecf9f30ebce2c772
Patrick Sheil <mrpasheil@gmail.com>
2022-10-24 19:44:10 -0300

    Add SignUp button and Home NavMenu icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <h2>Disney Plus Clone</h2> */}
+
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          {/* <Route path="/login" element={<Login />} />
+        <Route path="/checkout" element={[<Header />, <Checkout />]} />
+        <Route path="/payment" element={[<Header />, <Elements stripe={promise}><Payment /></Elements>]} />
+        <Route path="/orders" element={[<Header />, <Orders />]} />
+        <Route path="/" element={[<Header />, <Home />]} /> */}
+
+          {/* <Route path="*" element={<ErrorPage />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
