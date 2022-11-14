8ac3943b4acb0e0569585c36f5ced01449ca5fc5
Patrick Sheil <mrpasheil@gmail.com>
2022-11-14 18:25:51 -0300

    fix viewers video margin

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -3,36 +3,39 @@ import styled from "styled-components";
 const Viewers = (props) => {
   return (
     <Container>
-        <Wrap>
-            <img src="/images/viewers-disney.png" alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} muted>
-                <source src='/videos/1564674844-disney.mp4' type='video/mp4' />
-            </video>
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-pixar.png" alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} muted>
-                <source src='/videos/1564676714-pixar.mp4' type='video/mp4' />
-            </video>
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-marvel.png" alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} muted>
-                <source src='/videos/1564676115-marvel.mp4' type='video/mp4' />
-            </video>
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-starwars.png" alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} muted>
-                <source src='/videos/1608229455-star-wars.mp4' type='video/mp4' />
-            </video>
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-national.png" alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} muted>
-                <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4' />
-            </video>
-        </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Wrap>
     </Container>
   );
 };
@@ -46,14 +49,15 @@ const Container = styled.div`
   grid-template-columns: repeat(5, minmax(0, 1fr));
 
   @media (max-width: 768px) {
-    grid-template-columns: repeat(1, minmax(0,1fr));
+    grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 `;
 
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
@@ -74,18 +78,20 @@ const Wrap = styled.div`
   }
 
   video {
-    width: 100%;
-    hight:100%;
+    // width: 100%;
+    height: 100%;
     position: absolute;
     top: 0px;
     opacity: 0;
     z-index: 0;
+    transform: scale(1.05);
   }
 
   &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
 
-      transform: scale(1.05);
+    transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
 
     video {
